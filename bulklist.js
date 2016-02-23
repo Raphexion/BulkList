e03590a966762743322c78614a10dd36be2c6da2
Niklas Johansson <niklas.johanssonext@delaval.com>
2016-02-23 17:32:35 +0100

    bulklist.js: start writing keypress listener

--- bulklist.js
+++ bulklist.js
@@ -2,6 +2,8 @@
   var bulklist = {};
 
   bulklist.attach_to = function (document, div_id) {
+    var words = [];
+
     var div = document.getElementById(div_id);
     var list = document.createElement("ul");
     var bulk = document.createElement("textarea");
@@ -9,6 +11,15 @@
     bulk.rows = "80";
     div.appendChild(list);
     div.appendChild(bulk);
+
+    function update() {
+      var lines = bulk.value.split('\n');
+      for (var ii=0; ii<lines.length; ii++) {
+        console.log(lines[ii]);
+      }
+    };
+
+    bulk.addEventListener('keypress', update);
   };
 
   root.bulklist = bulklist;
