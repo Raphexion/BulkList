4469b6b47ef1de95b378af7dc68792fb5cb662ea
Magnus Lindhult <magnus.lindhult@headcraft.se>
2016-02-23 21:48:50 +0100

    Changed inner working of bulklist
    The bulklist now uses a revealing module pattern.
    Also added a .gitignore file.

--- bulklist.js
+++ bulklist.js
@@ -1,26 +1,48 @@
 (function (root) {
-  var bulklist = {};
+    'use strict';
 
-  bulklist.attach_to = function (document, div_id) {
-    var words = [];
+    var words = [],
+        app,
+        listContainer,
+        list,
+        bulk;
 
-    var div = document.getElementById(div_id);
-    var list = document.createElement("ul");
-    var bulk = document.createElement("textarea");
-    bulk.cols = "100";
-    bulk.rows = "80";
-    div.appendChild(list);
-    div.appendChild(bulk);
+    function attach_to(document, div_id) {
+        app = document.getElementById(div_id);
+        listContainer = document.createElement("div");
+        list = document.createElement("ul");
+        bulk = document.createElement("textarea");
+        bulk.cols = "100";
+        bulk.rows = "80";
+        listContainer.appendChild(list);
+        app.appendChild(listContainer);
+        app.appendChild(bulk);
+    }
+
+    function clearList() {
+        listContainer.removeChild(list);
+        list = document.createElement("ul");
+        listContainer.appendChild(list);
+    }
 
     function update() {
-      var lines = bulk.value.split('\n');
-      for (var ii=0; ii<lines.length; ii++) {
-        console.log(lines[ii]);
-      }
+        var lines = bulk.value.trim().split('\n'),
+            ii,
+            li;
+
+        clearList();
+
+        for (ii = 0; ii < lines.length; ii++) {
+            li = document.createElement('li');
+            li.innerHTML = lines[ii];
+            list.appendChild(li);
+        }
+    }
+
+    var bulklist = {
+        attach_to: attach_to,
+        update: update
     };
 
-    bulk.addEventListener('keypress', update);
-  };
-
-  root.bulklist = bulklist;
+    root.bulklist = bulklist;
 }(this));
